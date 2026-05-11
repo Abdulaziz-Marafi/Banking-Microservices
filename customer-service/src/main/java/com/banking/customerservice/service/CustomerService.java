9df7a27bca50e7c3fbc4b8c380ec05ce6e8d69ca
Abdulaziz-Marafi <azizbm4798@gmail.com>
2026-05-11 14:42:39 +0300

    Added Customerservice interface

--- customer-service/src/main/java/com/banking/customerservice/service/CustomerService.java
+++ customer-service/src/main/java/com/banking/customerservice/service/CustomerService.java
@@ -1,4 +1,14 @@
 package com.banking.customerservice.service;
 
+import com.banking.customerservice.dto.CreateCustomerRequest;
+import com.banking.customerservice.dto.CreateCustomerResponse;
+
+import java.util.List;
+
 public interface CustomerService {
+    CreateCustomerResponse createCustomer(CreateCustomerRequest request);
+    CreateCustomerResponse getCustomer(Long customerId);
+    List<CreateCustomerResponse> getAllCustomers();
+    CreateCustomerResponse updateCustomer (Long customerId, CreateCustomerRequest request);
+    void deleteCustomer(Long customerId);
 }
